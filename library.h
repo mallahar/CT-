662d57111206d8aa15c92a4a11b3ba616fcc2c11
mallahar <39564315+mallahar@users.noreply.github.com>
2018-06-08 11:22:18 -0400

    Add files via upload

---
+++ library.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <conio.h>
+#include <time.h>
+#include <stdio.h>
+#include <new>
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <windows.h>
+#include <cstdlib>
+#include <time.h>
+#include <vector>
+using namespace std;   
